7462891ee51912a425aa6905aca457e0b7837563
CTTACM <1247846793@qq.com>
2018-11-02 21:26:16 +0800

    Update leptjson.c

--- tutorial03/leptjson.c
+++ tutorial03/leptjson.c
@@ -1,3 +1,4 @@
+// 头文件的后面用注释写上要使用到的头文件中的函数
 #include "leptjson.h"
 #include <assert.h>  /* assert() */
 #include <errno.h>   /* errno, ERANGE */
@@ -5,14 +6,17 @@
 #include <stdlib.h>  /* NULL, malloc(), realloc(), free(), strtod() */
 #include <string.h>  /* memcpy() */
 
+// #ifndef /.../ #endif 结构，防止变量名重定义
 #ifndef LEPT_PARSE_STACK_INIT_SIZE
 #define LEPT_PARSE_STACK_INIT_SIZE 256
 #endif
 
+//注意宏的用法
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
 #define ISDIGIT(ch)         ((ch) >= '0' && (ch) <= '9')
 #define ISDIGIT1TO9(ch)     ((ch) >= '1' && (ch) <= '9')
 #define PUTC(c, ch)         do { *(char*)lept_context_push(c, sizeof(char)) = (ch); } while(0)
+
 
 typedef struct {
     const char* json;
@@ -20,6 +24,22 @@ typedef struct {
     size_t size, top;
 }lept_context;
 
+/*堆栈数据结构：
+//1.https://www.cnblogs.com/wangqiang3311/p/5976635.html
+//2.https://baike.baidu.com/item/%E5%A0%86%E6%A0%88/1682032
+// >>右移运算符“>>”是双目运算符。右移n位就是除以2的n次方
+*/
+// realloc：更改已经配置的内存空间 https://blog.csdn.net/hackerain/article/details/7954006
+// 为什么是（char*）realloc(c->stack, c->size) ，为什么是char*??同下：
+/* malloc 的用法
+(分配类型 *)malloc(分配元素个数 *sizeof(分配类型)) 如果成功，则返回该空间首地址，该空间没有初始化，如果失败，则返回0
+*/
+// void* 可以接受任何类型的赋值：https://www.cnblogs.com/yuanyongbin/p/8058755.html
+//c->size:栈当前所拥有的大小，而top指示的是当前已经填写的内存的位置。所以说c->size 一般是要比c->top大
+//size：需要入栈的内容所占内存大小
+//top：当前写入的内存的位置
+//stack：栈指针
+//栈扩容函数
 static void* lept_context_push(lept_context* c, size_t size) {
     void* ret;
     assert(size > 0);
@@ -40,6 +60,9 @@ static void* lept_context_pop(lept_context* c, size_t size) {
     return c->stack + (c->top -= size);
 }
 
+// 代表数组的指针，到底是代表了第一个值还是代表了整个数组的值：答案是代表了开头的字符，那么怎么表示整个字符串呢？
+// 代表数组的第一个，不是代表整个数组
+// 像"\t"这样的转义字符其实是一个字符，占一个字节的内存。
 static void lept_parse_whitespace(lept_context* c) {
     const char *p = c->json;
     while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r')
@@ -47,6 +70,8 @@ static void lept_parse_whitespace(lept_context* c) {
     c->json = p;
 }
 
+// 又返回值的函数，返回值基本都是判断函数指向情况的，较少有传值的。赋值都是在函数体中语句执行
+// 解析字符串中值的部分是true、false或者null时，用这个函数处理
 static int lept_parse_literal(lept_context* c, lept_value* v, const char* literal, lept_type type) {
     size_t i;
     EXPECT(c, literal[0]);
@@ -58,6 +83,15 @@ static int lept_parse_literal(lept_context* c, lept_value* v, const char* litera
     return LEPT_PARSE_OK;
 }
 
+// ERANGE:一个c库宏，判断数学式是否有意义（比如0做被除数是没有意义的）。
+/*
+HUGE_VAL
+当函数的结果不可以表示为浮点数时。如果是因为结果的幅度太大以致于无法表示，则函数会设置 errno 为 ERANGE 来表示范围错误，并返回一个由宏 HUGE_VAL 或者它的否定（- HUGE_VAL）命名的一个特定的很大的值。
+如果结果的幅度太小，则会返回零值。在这种情况下，error 可能会被设置为 ERANGE，也有可能不会被设置为 ERANGE。
+*/
+//http://www.runoob.com/cprogramming/c-macro-erange.html
+//https://zh.cppreference.com/w/c/numeric/math/HUGE_VAL
+// 解析数字类型的值
 static int lept_parse_number(lept_context* c, lept_value* v) {
     const char* p = c->json;
     if (*p == '-') p++;
@@ -78,7 +112,7 @@ static int lept_parse_number(lept_context* c, lept_value* v) {
         for (p++; ISDIGIT(*p); p++);
     }
     errno = 0;
-    v->u.n = strtod(c->json, NULL);
+    v->u.n = strtod(c->json, NULL);// 字符串转数字：https://blog.csdn.net/kongshuai19900505/article/details/51914035
     if (errno == ERANGE && (v->u.n == HUGE_VAL || v->u.n == -HUGE_VAL))
         return LEPT_PARSE_NUMBER_TOO_BIG;
     v->type = LEPT_NUMBER;
@@ -86,15 +120,17 @@ static int lept_parse_number(lept_context* c, lept_value* v) {
     return LEPT_PARSE_OK;
 }
 
+// "a\"b"，这个字符串的占用4个字节，拥有三个字符。所以中间的转义字符和双引号算作一个字符。
+//解析值是字符串类型:把“字符串值”放到缓冲区
 static int lept_parse_string(lept_context* c, lept_value* v) {
     size_t head = c->top, len;
     const char* p;
     EXPECT(c, '\"');
     p = c->json;
     for (;;) {
-        char ch = *p++;
-        switch (ch) {
-            case '\"':
+        char ch = *p++;  // 为什么每次都要重新申请一个变量？
+        switch (ch) {  
+            case '\"':   //下面语句并没有被大括号包围，所以应该是也是默认是一个语句块的
                 len = c->top - head;
                 lept_set_string(v, (const char*)lept_context_pop(c, len), len);
                 c->json = p;
@@ -119,6 +155,16 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
     }
 }
 
+// 主解析：解析整个传过来东西
+/*
+ret：用来反映解析情况
+1. 先初始化各个部分
+2. 去白
+3. 解析正确
+4. 去白
+5. 查看后面是否还有值
+6. 一些结束处理操作
+*/
 int lept_parse(lept_value* v, const char* json) {
     lept_context c;
     int ret;
@@ -140,6 +186,7 @@ int lept_parse(lept_value* v, const char* json) {
     return ret;
 }
 
+//只有在是字符串的时候才用得着free，释放完空间type变为null
 void lept_free(lept_value* v) {
     assert(v != NULL);
     if (v->type == LEPT_STRING)
@@ -179,7 +226,7 @@ size_t lept_get_string_length(const lept_value* v) {
     assert(v != NULL && v->type == LEPT_STRING);
     return v->u.s.len;
 }
-
+//申请空间+初始化成员变量
 void lept_set_string(lept_value* v, const char* s, size_t len) {
     assert(v != NULL && (s != NULL || len == 0));
     lept_free(v);
